bdff6d248e9b31effdca4cd67cc601c95c7c5919
james nelson <email@example.com>
2020-03-19 02:52:21 -0500

    cleanup

--- StateObserver.js
+++ StateObserver.js
@@ -1,77 +1,72 @@
 import * as views from "./components/views";
-import { Header, Nav, Main, Footer } from "./components";
+import { Header, Nav, Footer } from "./components";
 import * as stateStore from "./store";
 
-export class StateObserver{
-    constructor(subject, stateContext){
+export class StateObserver {
+    constructor(subject, stateContext) {
         this.subject = subject;
         this.stateContext = stateContext;
         this.active = (subject.state.view == stateContext.view);
         subject.registerObserver(this);
     }
 
-    notify(state){
-        if(state.view == this.stateContext.view){
-            this.active =  true;
+    notify(state) {
+        if (state.view == this.stateContext.view) {
+            this.active = true;
         } else {
-            this.active =  false;
+            this.active = false;
         }
         this.render();
     }
 
-    render(){
-        //let result = '';
-        if(this.active == true){
-           // result = `${views[this.stateContext.view](this.stateContext)}`;
-        //}
-        //return result;
-        document.querySelector("#root").innerHTML = `
-        ${Header(this.stateContext)}
-        ${Nav(stateStore.Links)}
-        ${views[this.stateContext.view](this.stateContext)}
-        ${Footer()}`;
+    render() {
+        if (this.active == true) {
+            document.querySelector("#root").innerHTML = `
+                ${Header(this.stateContext)}
+                ${Nav(stateStore.Links)}
+                ${views[this.stateContext.view](this.stateContext)}
+                ${Footer()}
+            `;
 
-        this.addNavEventListeners();
-        this.addPicOnFormSubmit(this.stateContext);
+            this.addNavEventListeners();
+            this.addPicOnFormSubmit(this.stateContext);
         }
     }
 
     addNavEventListeners() {
         // add event listeners to Nav items for navigation
         document.querySelectorAll("nav a").forEach(navLink =>
-          navLink.addEventListener("click", event => {
-            event.preventDefault();
-            this.subject.setState(stateStore[event.target.title]);
-          })
+            navLink.addEventListener("click", event => {
+                event.preventDefault();
+                this.subject.setState(stateStore[event.target.title]);
+            })
         );
         // add menu toggle to bars icon in nav bar
         document
-          .querySelector(".fa-bars")
-          .addEventListener("click", () =>
-            document.querySelector("nav > ul").classList.toggle("hidden--mobile")
-          );
-      }
-      
-      addPicOnFormSubmit(st) {
+            .querySelector(".fa-bars")
+            .addEventListener("click", () =>
+                document.querySelector("nav > ul").classList.toggle("hidden--mobile")
+            );
+    }
+
+    addPicOnFormSubmit(st) {
         if (st.view === "Form") {
-          document.querySelector("form").addEventListener("submit", event => {
-            event.preventDefault();
-            // convert HTML elements to Array
-            let inputList = Array.from(event.target.elements);
-            // remove submit button from list
-            inputList.pop();
-            // construct new picture object
-            let newPic = inputList.reduce((pictureObject, input) => {
-              pictureObject[input.name] = input.value;
-              return pictureObject;
-            }, {});
-            // add new picture to state.Gallery.pictures
-            state.Gallery.pictures.push(newPic);
-            render(state.Gallery);
-          });
+            document.querySelector("form").addEventListener("submit", event => {
+                event.preventDefault();
+                // convert HTML elements to Array
+                let inputList = Array.from(event.target.elements);
+                // remove submit button from list
+                inputList.pop();
+                // construct new picture object
+                let newPic = inputList.reduce((pictureObject, input) => {
+                    pictureObject[input.name] = input.value;
+                    return pictureObject;
+                }, {});
+                // add new picture to state.Gallery.pictures
+                state.Gallery.pictures.push(newPic);
+                render(state.Gallery);
+            });
         }
-      }
+    }
 
 }
-
-//export default StateObserver;
--- index.js
+++ index.js
@@ -1,63 +1,12 @@
-//import { Header, Nav, Main, Footer } from "./components";
 import * as state from "./store";
 import {StateSubject} from "./StateSubject"
 import {StateObserver} from "./StateObserver"
 
 const appState = new StateSubject(state.Home);
 const pages = [];
-pages.push(new StateObserver(appState,state.Bio));
-pages.push(new StateObserver(appState,state.Form));
-pages.push(new StateObserver(appState,state.Gallery));
-pages.push(new StateObserver(appState,state.Home));
+pages.push(new StateObserver(appState, state.Bio));
+pages.push(new StateObserver(appState, state.Form));
+pages.push(new StateObserver(appState, state.Gallery));
+pages.push(new StateObserver(appState, state.Home));
 
 appState.setState(state.Home);
-
-/*function render(st = state.Home) {
-  document.querySelector("#root").innerHTML = `
-  ${Header(st)}
-  ${Nav(state.Links)}
-  ${pages.map(page => page.render()).join('')}
-  ${Footer()}
-`;
-  addNavEventListeners();
-  addPicOnFormSubmit(st);
-}
-
-render();*/
-
-/*function addNavEventListeners() {
-  // add event listeners to Nav items for navigation
-  document.querySelectorAll("nav a").forEach(navLink =>
-    navLink.addEventListener("click", event => {
-      event.preventDefault();
-      appState.setState(state[event.target.title]);
-      render(state[event.target.title]);
-    })
-  );
-  // add menu toggle to bars icon in nav bar
-  document
-    .querySelector(".fa-bars")
-    .addEventListener("click", () =>
-      document.querySelector("nav > ul").classList.toggle("hidden--mobile")
-    );
-}
-
-function addPicOnFormSubmit(st) {
-  if (st.view === "Form") {
-    document.querySelector("form").addEventListener("submit", event => {
-      event.preventDefault();
-      // convert HTML elements to Array
-      let inputList = Array.from(event.target.elements);
-      // remove submit button from list
-      inputList.pop();
-      // construct new picture object
-      let newPic = inputList.reduce((pictureObject, input) => {
-        pictureObject[input.name] = input.value;
-        return pictureObject;
-      }, {});
-      // add new picture to state.Gallery.pictures
-      state.Gallery.pictures.push(newPic);
-      render(state.Gallery);
-    });
-  }
-} */
